224b117aa750a892385f9c7b8eac803300926ba0
Edward Liu <shiqiu1105@gmail.com>
2016-06-20 17:29:41 -0700

    Making vr.LensMatchedShadingResScale works correctly with Vive HMD.

--- Engine/Plugins/Runtime/Steam/SteamVR/Source/SteamVR/Private/SteamVRHMD.cpp
+++ Engine/Plugins/Runtime/Steam/SteamVR/Source/SteamVR/Private/SteamVRHMD.cpp
@@ -1035,6 +1035,20 @@ void FSteamVRHMD::CalculateRenderTargetSize(const class FViewport& Viewport, uin
 	{
 		static const auto CVar = IConsoleManager::Get().FindTConsoleVariableDataFloat(TEXT("r.ScreenPercentage"));
 		float value = CVar->GetValueOnGameThread();
+
+		static const auto CVarLensMatchedShading = IConsoleManager::Get().FindTConsoleVariableDataInt(TEXT("vr.LensMatchedShading"));
+		static const auto CVarLensMatchedShadingRendering = IConsoleManager::Get().FindTConsoleVariableDataInt(TEXT("vr.LensMatchedShadingRendering"));
+		bool bLensMatchedShadeEnabled = GSupportsFastGeometryShader && GSupportsModifiedW &&
+			CVarLensMatchedShading && CVarLensMatchedShading->GetValueOnGameThread() && CVarLensMatchedShadingRendering->GetValueOnGameThread() > 0;
+
+		if (bLensMatchedShadeEnabled)
+		{
+			static const auto CVarLensMatchedShadingResScale = IConsoleManager::Get().FindTConsoleVariableDataFloat(TEXT("vr.LensMatchedShadingResolutionScaling"));
+			float LensMatchedShadeResScale = CVarLensMatchedShadingResScale->GetValueOnGameThread();
+
+			value *= LensMatchedShadeResScale;
+		}
+
 		if (value > 0.0f)
 		{
 			InOutSizeX = FMath::CeilToInt(InOutSizeX * value / 100.f);
--- Engine/Source/Runtime/Engine/Private/SceneView.cpp
+++ Engine/Source/Runtime/Engine/Private/SceneView.cpp
@@ -2206,19 +2206,16 @@ void FSceneView::SetupVRProjection(int32 ViewportGap)
 		// Hard-coded for now!
 		LensMatchedShadingConf = FLensMatchedShading::Configuration_Vive;
 
-		// Scale the conf to get proper display on non-HMD display too
-		if (!GEngine->IsStereoscopic3D() || GEngine->StereoRenderingDevice->IsEmulatedStereo())
-		{
-			int SizeX = LensMatchedShadingConf.SizeLeft + LensMatchedShadingConf.SizeRight;
-			int SizeY = LensMatchedShadingConf.SizeUp + LensMatchedShadingConf.SizeDown;
-			float ScaleX = ViewRect.Width() / float(SizeX);
-			float ScaleY = ViewRect.Height() / float(SizeY);
+		// Scale the conf to get proper display on non-HMD display and also handles screen percentage not being 100%
+		int SizeX = LensMatchedShadingConf.SizeLeft + LensMatchedShadingConf.SizeRight;
+		int SizeY = LensMatchedShadingConf.SizeUp + LensMatchedShadingConf.SizeDown;
+		float ScaleX = ViewRect.Width() / float(SizeX);
+		float ScaleY = ViewRect.Height() / float(SizeY);
 
-			LensMatchedShadingConf.SizeLeft *= ScaleX;
-			LensMatchedShadingConf.SizeRight *= ScaleX;
-			LensMatchedShadingConf.SizeUp *= ScaleY;
-			LensMatchedShadingConf.SizeDown *= ScaleY;
-		}
+		LensMatchedShadingConf.SizeLeft *= ScaleX;
+		LensMatchedShadingConf.SizeRight *= ScaleX;
+		LensMatchedShadingConf.SizeUp *= ScaleY;
+		LensMatchedShadingConf.SizeDown *= ScaleY;
 
 		// round locations before mirroring
 		FIntRect OriginalViewport = ViewRect;
