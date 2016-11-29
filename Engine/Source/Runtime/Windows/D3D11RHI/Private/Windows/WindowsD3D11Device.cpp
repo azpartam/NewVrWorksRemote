azpartam/NewVrWorksRemote

commit af42c77b026fa1c06e3bb63ae7267a73740d0efd

Add GPU detection before getting multi-gpu caps.

Engine/Source/Runtime/Windows/D3D11RHI/Private/Windows/WindowsD3D11Device.cpp
@@ -623,10 +623,15 @@ void FD3D11DynamicRHI::InitD3DDevice()
 		}
 
 		NV_MULTIGPU_CAPS MultiGPUCaps;
-		VERIFYD3D11RESULT(NvAPI_D3D11_MultiGPU_GetCaps(&MultiGPUCaps));
-		// Enable VR-SLI driver layer, before device is created
-		// Note this should eventually be a render property, since it is selecting an explicit MGPU model.
-		VERIFYD3D11RESULT(NvAPI_D3D11_MultiGPU_Init(true));
+#if PLATFORM_DESKTOP
+		if (IsRHIDeviceNVIDIA())
+		{
+			VERIFYD3D11RESULT(NvAPI_D3D11_MultiGPU_GetCaps(&MultiGPUCaps));
+			// Enable VR-SLI driver layer, before device is created
+			// Note this should eventually be a render property, since it is selecting an explicit MGPU model.
+			VERIFYD3D11RESULT(NvAPI_D3D11_MultiGPU_Init(true));
+		}
+#endif
 
 		D3D_FEATURE_LEVEL ActualFeatureLevel = (D3D_FEATURE_LEVEL)0;
 

commit fdc1969ed58c52452486a1df3b16d48935c69f6e

fix the warning caused by Commit af42c77

Engine/Source/Runtime/Windows/D3D11RHI/Private/Windows/WindowsD3D11Device.cpp
@@ -621,8 +621,9 @@ void FD3D11DynamicRHI::InitD3DDevice()
 		{
 			check(!"Internal error, EnumAdapters() failed but before it worked")
 		}
-
+		
 		NV_MULTIGPU_CAPS MultiGPUCaps;
+		ZeroMemory(&MultiGPUCaps, sizeof(MultiGPUCaps));
 #if PLATFORM_DESKTOP
 		if (IsRHIDeviceNVIDIA())
 		{